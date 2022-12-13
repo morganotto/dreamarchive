0b58044feac6eaf09ff56fa1f90bd9d1231161e1
morganotto <100333716+morganotto@users.noreply.github.com>
2022-12-12 17:34:03 -0800

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,115 @@
+
+
+var post= document.getElementById("remember1");
+post.addEventListener("click", function(){
+    var commentBoxValue= document.getElementById("comment-box1").value;
+ 
+    var li = document.createElement("li");
+    var text = document.createTextNode(commentBoxValue);
+    li.appendChild(text);
+    document.getElementById("unordered1").appendChild(li);
+
+});
+
+
+
+
+var post= document.getElementById("remember2");
+post.addEventListener("click", function(){
+    var commentBoxValue= document.getElementById("comment-box2").value;
+ 
+    var li = document.createElement("li");
+    var text = document.createTextNode(commentBoxValue);
+    li.appendChild(text);
+    document.getElementById("unordered2").appendChild(li);
+ 
+});
+
+var post= document.getElementById("remember3");
+post.addEventListener("click", function(){
+    var commentBoxValue= document.getElementById("comment-box3").value;
+ 
+    var li = document.createElement("li");
+    var text = document.createTextNode(commentBoxValue);
+    li.appendChild(text);
+    document.getElementById("unordered3").appendChild(li);
+ 
+});
+
+var post= document.getElementById("remember4");
+post.addEventListener("click", function(){
+    var commentBoxValue= document.getElementById("comment-box4").value;
+ 
+    var li = document.createElement("li");
+    var text = document.createTextNode(commentBoxValue);
+    li.appendChild(text);
+    document.getElementById("unordered4").appendChild(li);
+ 
+});
+
+var post= document.getElementById("remember5");
+post.addEventListener("click", function(){
+    var commentBoxValue= document.getElementById("comment-box5").value;
+ 
+    var li = document.createElement("li");
+    var text = document.createTextNode(commentBoxValue);
+    li.appendChild(text);
+    document.getElementById("unordered5").appendChild(li);
+ 
+});
+
+var post= document.getElementById("remember6");
+post.addEventListener("click", function(){
+    var commentBoxValue= document.getElementById("comment-box6").value;
+ 
+    var li = document.createElement("li");
+    var text = document.createTextNode(commentBoxValue);
+    li.appendChild(text);
+    document.getElementById("unordered6").appendChild(li);
+ 
+});
+
+var post= document.getElementById("remember7");
+post.addEventListener("click", function(){
+    var commentBoxValue= document.getElementById("comment-box7").value;
+ 
+    var li = document.createElement("li");
+    var text = document.createTextNode(commentBoxValue);
+    li.appendChild(text);
+    document.getElementById("unordered7").appendChild(li);
+ 
+});
+
+var post= document.getElementById("remember8");
+post.addEventListener("click", function(){
+    var commentBoxValue= document.getElementById("comment-box8").value;
+ 
+    var li = document.createElement("li");
+    var text = document.createTextNode(commentBoxValue);
+    li.appendChild(text);
+    document.getElementById("unordered8").appendChild(li);
+ 
+});
+
+var post= document.getElementById("remember9");
+post.addEventListener("click", function(){
+    var commentBoxValue= document.getElementById("comment-box9").value;
+ 
+    var li = document.createElement("li");
+    var text = document.createTextNode(commentBoxValue);
+    li.appendChild(text);
+    document.getElementById("unordered9").appendChild(li);
+ 
+});
+
+var post= document.getElementById("remember10");
+post.addEventListener("click", function(){
+    var commentBoxValue= document.getElementById("comment-box10").value;
+ 
+    var li = document.createElement("li");
+    var text = document.createTextNode(commentBoxValue);
+    li.appendChild(text);
+    document.getElementById("unordered10").appendChild(li);
+ 
+});
+
